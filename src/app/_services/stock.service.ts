d9c6bc6340e5318ca5c9a0ceb47ce22a99a992b4
pmutua <pmutua@live.com>
2021-10-11 15:12:38 +0300

    Added service to consume quandl API

---
+++ src/app/_services/stock.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  private headers = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+ 
+
+  constructor(public http: HttpClient) { }
+
+
+  getEndofDayUSStockPrices(ticker: string) {
+    const url = '/datasets/WIKI/'+`${ticker}`+'/data.json?api_key='+`${environment.quandiAPIKey}`;
+    return this.http.get<any>(`${environment.quandiBaseUrl}`+`${url}`, this.headers);
+  }
+
+  
+
+}
+
+
+
